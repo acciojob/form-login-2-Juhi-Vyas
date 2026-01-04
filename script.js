b9df22db42af82422994e937862426fd6de36ac0
Juhi-Vyas <125583903+Juhi-Vyas@users.noreply.github.com>
2026-01-04 23:56:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+function showAlert(e) {
+      e.preventDefault(); // stop page refresh
+
+      let fname = document.getElementById("fname").value;
+      let lname = document.getElementById("lname").value;
+      let phone = document.getElementById("phone").value;
+      let email = document.getElementById("email").value;
+
+      alert(
+        "First Name: " + fname + "\n" +
+        "Last Name: " + lname + "\n" +
+        "Phone Number: " + phone + "\n" +
+        "Email ID: " + email
+      );
+    }
